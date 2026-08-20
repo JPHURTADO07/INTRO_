ae38d32fbd12a469316316f3ea543534096645a6
Juan Pablo Orrego Hurtado <44008914+JPHURTADO07@users.noreply.github.com>
2026-08-20 13:27:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfac
 st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open('be alone.jpg')
 st.image (image, caption='be alone')
+
+texto = st.text_input('Escribe algo', "Este es mi texto')
+st.write('El texto escrito es', texto)
